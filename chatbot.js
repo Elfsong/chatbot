e8f015c360a14d868846ae71296d78d3d811b156
Du Mingzhe <mingzhe@nus.edu.sg>
2023-03-15 23:03:56 +0800

    lalala

--- chatbot.js
+++ chatbot.js
@@ -1,3 +1,6 @@
+// Conversation History
+var conversation_list = [];
+
 // Get chatbot elements
 const chatbot = document.getElementById('chatbot');
 const conversation = document.getElementById('conversation');
@@ -25,38 +28,26 @@ inputForm.addEventListener('submit', function(event) {
   // Generate chatbot response
   const response = generateResponse(input);
 
-  // Add chatbot response to conversation
-  message = document.createElement('div');
-  message.classList.add('chatbot-message','chatbot');
-  message.innerHTML = `<p class="chatbot-text" sentTime="${currentTime}">${response}</p>`;
-  conversation.appendChild(message);
-  message.scrollIntoView({behavior: "smooth"});
+  
 });
 
 // Generate chatbot response function
-function generateResponse(input) {
-    // Add chatbot logic here
-    const responses = [
-      "Hello, how can I help you today? 😊",
-      "I'm sorry, I didn't understand your question. Could you please rephrase it? 😕",
-      "I'm here to assist you with any questions or concerns you may have. 📩",
-      "I'm sorry, I'm not able to browse the internet or access external information. Is there anything else I can help with? 💻",
-      "What would you like to know? 🤔",
-      "I'm sorry, I'm not programmed to handle offensive or inappropriate language. Please refrain from using such language in our conversation. 🚫",
-      "I'm here to assist you with any questions or problems you may have. How can I help you today? 🚀",
-      "Is there anything specific you'd like to talk about? 💬",
-      "I'm happy to help with any questions or concerns you may have. Just let me know how I can assist you. 😊",
-      "I'm here to assist you with any questions or problems you may have. What can I help you with today? 🤗",
-      "Is there anything specific you'd like to ask or talk about? I'm here to help with any questions or concerns you may have. 💬",
-      "I'm here to assist you with any questions or problems you may have. How can I help you today? 💡",
-    ];
+async function generateResponse(input) {
+  conversation_list.push({"role": "user", "content": input});
 
-    fetch('https://nlp-platform.online/whatsapp_bot/chat?token=mIngzhe666!&conversation_list=[{"role": "user", "content": "Hello!"}]')
-		.then((response) => response.json())
-		.then((json) => console.log(json));
-    
-    // Return a random response
-    return responses[Math.floor(Math.random() * responses.length)];
-  }
+  let response = await fetch('https://nlp-platform.online/whatsapp_bot/chat?token=mIngzhe666!&conversation_list=' + JSON.stringify(conversation_list));
+  var data = await response.json();    
+  conversation_list = data.conversation_list;
+
+  let result = conversation_list.slice(-1)[0].content;
+  const currentTime = new Date().toLocaleTimeString([], { hour: '2-digit', minute: "2-digit" });
+
+  // Add chatbot response to conversation
+  message = document.createElement('div');
+  message.classList.add('chatbot-message','chatbot');
+  message.innerHTML = `<p class="chatbot-text" sentTime="${currentTime}">${result}</p>`;
+  conversation.appendChild(message);
+  message.scrollIntoView({behavior: "smooth"});
+}
   
   
